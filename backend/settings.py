13f790e7cb534968f519b1e12ec47dee90b281e4
Jordi <jordi8101@gmail.com>
2021-03-19 00:16:23 -0700

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -180,6 +180,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ALLOW_ALL_ORIGINS = True
 
