7b5273f1a9797c454e8fe5ee083d6e1d69c3eb72
JordiEspinozaMendoza <71615543+JordiEspinozaMendoza@users.noreply.github.com>
2021-03-20 01:33:38 -0700

    Update Login.js

--- frontend/static/js/Login.js
+++ frontend/static/js/Login.js
@@ -1,13 +1,15 @@
 var formLogin = document.getElementById("formLogin");
-
+//Evento submit del formulario
 formLogin.addEventListener("submit", function (e) {
   e.preventDefault();
+  //Data a enviar
   let data = {
     username: this.username.value,
     password: this.password.value,
   };
 
   // console.log(data);
+  //Enviamos al backend
   let loginPost = new AccessToken();
   loginPost
     .post(mainUrl+"api/users/login/", data);
