0675b7ea3999a1a7f66f7e277f612b7e92f39ef7
JordiEspinozaMendoza <71615543+JordiEspinozaMendoza@users.noreply.github.com>
2021-03-20 01:32:53 -0700

    Update FoodUpload.js

--- frontend/static/js/FoodUpload.js
+++ frontend/static/js/FoodUpload.js
@@ -1,15 +1,15 @@
 if(localStorage.getItem("access") == null && localStorage.getItem("userInfo")==null){
-  window.location.href = mainUrl;
+  window.location.href = mainUrl; //Si el usuario no esta logueado lo regresamos
 }
-
+//Formulario
 let form = document.getElementById("foodUpload");
-
+//evento submit
 form.addEventListener("submit", function (e) {
   let responseMessage = document.getElementById("responseMessage");
   responseMessage.innerHTML = `<span class = "message message-await">Subiendo comida...</span>`
   e.preventDefault();
   let access =  localStorage.getItem("access");
-
+  //Data a enviar
   let data = {
     name: form.name.value,
     description: form.description.value,
@@ -23,18 +23,21 @@ form.addEventListener("submit", function (e) {
     .then((data) => console.log(data))
     .catch((err) => console.log(err));
 });
-
+//Input con la imagen
 var inputImage = document.getElementById("inputImage");
+//Evento change
 inputImage.addEventListener("change", function () {
+  //Mensaje de espera
   let responseMessage = document.getElementById("responseMessage");
   responseMessage.innerHTML = `<span class = "message message-await">Subiendo imagen...</span>`
   let sendImage = new UploadImage();
   console.log(inputImage.files[0]);
-
+  
+  //Form data a enviar
   var formData = new FormData();
   formData.append("image", inputImage.files[0]);
   let access =  localStorage.getItem("access");
-  
+  //Enviamos al backend
   sendImage
     .post(mainUrl+"api/foods/uploadImage/", formData, access)
     .then((data) => {
