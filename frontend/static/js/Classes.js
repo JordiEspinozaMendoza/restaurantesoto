bea0ebf4b17a370ea09d019f3988c517b808ff45
JordiEspinozaMendoza <71615543+JordiEspinozaMendoza@users.noreply.github.com>
2021-03-20 01:20:12 -0700

    Update Classes.js

--- frontend/static/js/Classes.js
+++ frontend/static/js/Classes.js
@@ -1,16 +1,13 @@
 class CreateFood {
-  // Make an HTTP PUT Request
   async post(url, data) {
     const response = await fetch(url, {
       method: "POST",
       headers: {
         "X-CSRFToken": csrftoken,
         "Authorization": `Bearer ${data}`,
-
       },
     });
     const resData = await response.json();
-
     return resData;
   }
 }
@@ -29,15 +26,17 @@ class UploadFood {
     const resData = await response.json();
     let statusUpload = await response.status;
     if(statusUpload == 500){  
+      //Si el backend nos regresa un error
       let responseMessage = document.getElementById("responseMessage");
       responseMessage.innerHTML = `<span class = "message message-400">No se ha subido el alimento</span>`
     }else{
+      //En caso de que SI se suba la comida correctamente
       window.location.href = mainUrl+"foodList/"
       return resData;
     }
   }
 }
-
+//Class para eliminar alguna comida
 class DeleteFood {
   async delete(url, data) {
     const response = await fetch(url, {
@@ -53,6 +52,7 @@ class DeleteFood {
     return resData;
   }
 }
+//Class para subir una imagen
 class UploadImage {
   async post(url, formData, token) {
     console.log(token);
@@ -68,7 +68,7 @@ class UploadImage {
     return resData;
   }
 }
-
+//Class para actualizar una comida
 class EditFood {
   async put(url, data) {
     const response = await fetch(url, {
@@ -84,14 +84,17 @@ class EditFood {
     let statusUpload = await response.status;
     console.log(statusUpload);
     if(statusUpload == 500){  
+      //Si ocurre algun error en el backend
       let responseMessage = document.getElementById("responseMessage");
       responseMessage.innerHTML = `<span class = "message message-400">No se ha subido el alimento</span>`
     }else{
+      //Si no nos redirigimos a la pagina de comidas
       window.location.href = mainUrl+"foodList/"
       return resData;
     }
   }
 }
+//Actualizar imagen
 class UpdateImage {
   async put(url, formData, token) {
     console.log(formData);
@@ -108,7 +111,7 @@ class UpdateImage {
     return resData;
   }
 }
-
+//Class para obtener token de acceso que sera usado en los otros EP
 class AccessToken {
   async post(url, data) {
     const response = await fetch(url, {
@@ -120,12 +123,16 @@ class AccessToken {
     });
     const resData = await response.json();
     const status = await response.status;
+    
+    //En caso de que el usuario o la password sean incorrectos
     if(status == 401){  
       let responseMessage = document.getElementById("responseMessage");
       responseMessage.innerHTML = `<span class = "message message-400">Estas claves no pertenecen a ningún usuario</span>`
     }else{
+      //Guardamos el token en el LS
       localStorage.setItem("access", resData.access);
       const userData = new Login();
+      //Hacemos la llamada para acceder a los datos del usuario
       userData
         .get(mainUrl+"api/users/profile/", resData.access)
         .then((data) => console.log(data))
@@ -137,6 +144,7 @@ class AccessToken {
     return resData;
   }
 }
+//Class para obtener los datos del usuario administrador
 class Login {
   async get(url, data) {
     const response = await fetch(url, {
