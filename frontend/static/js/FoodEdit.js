2d538390fe8afe1204f2c30f13d3359d7d072866
JordiEspinozaMendoza <71615543+JordiEspinozaMendoza@users.noreply.github.com>
2021-03-20 01:23:15 -0700

    Update FoodEdit.js

--- frontend/static/js/FoodEdit.js
+++ frontend/static/js/FoodEdit.js
@@ -1,15 +1,21 @@
 if(localStorage.getItem("access") == null && localStorage.getItem("userInfo")==null){
-  window.location.href = mainUrl;
+  window.location.href = mainUrl; //En caso de que el usuario no este logueado
 }
+//Obtenemos el id de la comida desde los parametros de la url 
 let idFood = window.location.href.split("/")[4];
 
 let formEdit = document.getElementById("foodUploadEdit");
+
+//Evento submit del formulario
 formEdit.addEventListener("submit", function (e) {
+  //Mensaje de espera
   let responseMessage = document.getElementById("responseMessage");
   responseMessage.innerHTML = `<span class = "message message-await">Subiendo comida...</span>`
   e.preventDefault();
+  //Tomamos el token (en caso de tenerlo)
   let access =  localStorage.getItem("access");
   // console.log(idFood);
+  //JSON que sera enviado al back
   let data = {
     _id: idFood,
     name: formEdit.name.value,
@@ -26,16 +32,22 @@ formEdit.addEventListener("submit", function (e) {
     .catch((err) => console.log(err));
 });
 
+//Obtenemos la imagen a enviar
 var inputImageUpdate = document.getElementById("inputImageUpdate");
+
+//Evento change del input de comida
 inputImageUpdate.addEventListener("change", function () {
+  //Mensaje de espera
   let responseMessage = document.getElementById("responseMessage");
   responseMessage.innerHTML = `<span class = "message message-await">Subiendo imagen...</span>`
 
   let sendImage = new UpdateImage();
+  //Tomamos el token
   let access =  localStorage.getItem("access");
 
   console.log(inputImageUpdate.files[0]);
-
+  
+  //Creamos un formdata que es enviado al back
   var formData = new FormData();
   formData.append("image", inputImageUpdate.files[0]);
   formData.append("_id", idFood);
